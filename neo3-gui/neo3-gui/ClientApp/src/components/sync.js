f310d7aeafcc8fe25f9af91fef6155ee0180e8d1
Shuai <bfshm@qq.com>
2020-03-04 16:20:05 +0800

    websocket update

--- neo3-gui/neo3-gui/ClientApp/src/components/sync.js
+++ neo3-gui/neo3-gui/ClientApp/src/components/sync.js
@@ -1,65 +1,74 @@
 
 import React from 'react';
-import { Icon , Typography } from 'antd';
+import { Icon, Typography } from 'antd';
 import 'antd/dist/antd.css';
+import WsMessageComponent from './Websocket/WsMessageComponent';
 
 const { Text } = Typography;
 
-class Sync extends React.Component{
-  constructor(props){
+class Sync extends WsMessageComponent {
+  constructor(props) {
     super(props);
     this.state = {
-      height:0,
-      syncHeight:0
+      height: 0,
+      syncHeight: 0
     };
+
+    this.register((wsMessage) => this.setState({
+      height: wsMessage.result.headerHeight,
+      syncHeight: wsMessage.result.syncHeight
+    }));
   }
-  UNSAFE_componentWillMount(){
-    this.getConnect();
-  }
-  getConnect(){
-    let _this = this;
-    var ws = new WebSocket("ws://localhost:8081");
-    ws.onopen = function() {
-        console.log("this open");
-    };
-    
-    ws.onmessage = function(e) {
-        let data = JSON.parse(e.data).result;
-        console.log(data);
-        _this.getSyncheight(data.syncHeight);
-        _this.getHeight(data.headerHeight);
-    }
-    
-    ws.onclose = function(e) {
-        console.log(e);
-        console.log("closed");
-    }
-    
-    ws.onerror = function(e) {
-        console.log("error" + e);
-    }
-  }
-  getSyncheight(num){
-    this.setState({
-      syncHeight: num
-    })
-  }
-  getHeight(num){
-    this.setState({
-      height: num
-    })
-  }
-  render(){
+  // UNSAFE_componentWillMount(){
+  //   this.getConnect();
+  //   console.log("Sync connnecting!");
+
+  // }
+
+  // getConnect(){
+  //   let _this = this;
+  //   var ws = new WebSocket("ws://localhost:8081");
+  //   ws.onopen = function() {
+  //       console.log("this open");
+  //   };
+
+  //   ws.onmessage = function(e) {
+  //       let data = JSON.parse(e.data).result;
+  //       console.log(data);
+  //       _this.getSyncheight(data.syncHeight);
+  //       _this.getHeight(data.headerHeight);
+  //   }
+
+  //   ws.onclose = function(e) {
+  //       console.log(e);
+  //       console.log("closed");
+  //   }
+
+  //   ws.onerror = function(e) {
+  //       console.log("error" + e);
+  //   }
+  // }
+  // getSyncheight(num){
+  //   this.setState({
+  //     syncHeight: num
+  //   })
+  // }
+  // getHeight(num){
+  //   this.setState({
+  //     height: num
+  //   })
+  // }
+  render() {
     return (
       <div>
         <p>
-          <Icon type="sync" spin/> 
+          <Icon type="sync" spin />
           <Text type="secondary"> {this.state.syncHeight} / {this.state.height} </Text>
           <Text>区块同步中</Text>
         </p>
       </div>
     );
   }
-} 
+}
 
 export default Sync;
