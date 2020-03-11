da6f8549c8dfebce5cf8af3fb7ce5835fb406261
yanmingxia <18605540702@163.com>
2020-03-11 11:51:07 +0800

    点击加载更多

--- neo3-gui/neo3-gui/ClientApp/src/components/Chain/chain.js
+++ neo3-gui/neo3-gui/ClientApp/src/components/Chain/chain.js
@@ -26,17 +26,17 @@ class Chain extends React.Component{
         initLoading: false,
         data: res.result,
         blocklist: res.result,
-        lastblock: res.result[1]?res.result[1].blockHeight:null
+        lastblock: res.result[res.result.length-1].blockHeight-1
       },()=>{});
-    });
+    })
   }
   getBlock = callback => {
     console.log(this.state.lastblock)
     let _params = this.state.lastblock?{
-      "limit": 2,
+      "limit": 50,
       "height":this.state.lastblock
     }:{
-      "limit": 2
+      "limit": 50
     };
     axios.post('http://localhost:8081', {
       "id":"51",
@@ -61,7 +61,7 @@ class Chain extends React.Component{
   loadMore = () =>{
     this.setState({
       loading: true,
-      list: this.state.data.concat([...new Array(count)].map(() => ({ loading: true, name: {} }))),
+      blocklist: this.state.data.concat([...new Array(count)].map(() => ({ loading: true, name: {} }))),
     });
     this.getBlock(res => {
       const data = this.state.data.concat(res.result);
@@ -69,21 +69,23 @@ class Chain extends React.Component{
       this.setState(
         {
           data,
-          list: data,
+          blocklist: data,
           loading: false,
+          lastblock: data[data.length-1].blockHeight-1
         },
         () => {
           window.dispatchEvent(new Event('resize'));
         },
       );
     });
-
+  }
+  getAll(){
     var _this = this;
     axios.post('http://localhost:8081', {
       "id":"51",
       "method": "GetLastBlocks",
       "params": {
-        "limit": 50
+        "limit": 3
       }
     })
     .then(function (response) {
@@ -93,9 +95,6 @@ class Chain extends React.Component{
         message.error("查询失败");
         return;
       }
-      _this.setState({
-        blocklist:_data.result
-      })
     })
     .catch(function (error) {
       console.log(error);
@@ -106,15 +105,8 @@ class Chain extends React.Component{
     const {initLoading,loading,blocklist} = this.state;
     const loadMore =
       !initLoading && !loading ? (
-        <div
-          style={{
-            textAlign: 'center',
-            marginTop: 12,
-            height: 32,
-            lineHeight: '32px',
-          }}
-        >
-          <Button type="primary" onClick={this.loadMore}>loading more</Button>
+        <div className="text-c mb3">
+          <Button type="primary" onClick={this.loadMore}>加载更多</Button>
         </div>
       ) : null;
     return (
@@ -123,7 +115,6 @@ class Chain extends React.Component{
           <Row gutter={[30, 0]} type="flex" style={{ 'minHeight': 'calc( 100vh - 120px )'}}>
             <Col span={24} className="bg-white pv4">
             <Intitle content="区块列表"/>
-            <div ref="blockList">
               <List
                 header={<div><span>区块信息</span><span className="float-r ml4"><span className="wa-amount">数量</span></span><span className="float-r">更新时间</span></div>}
                 footer={<span></span>}
@@ -143,13 +134,9 @@ class Chain extends React.Component{
                 </List.Item>
                 )}
               />
-            </div>
-              <div className="mb4" >
-                  <a onClick={this.loadMore}>加载更多</a>
-              </div>
               </Col>
-              <div className="pv1"></div>
           </Row>
+          <div className="pv1"></div>
         </Content>
       </Layout>
     );
