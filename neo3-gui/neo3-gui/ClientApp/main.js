58e65ea66ae6880d9ad059d71cc33dd6c30e812a
Shuai <bfshm@qq.com>
2020-04-28 16:14:25 +0800

    single instance window

--- neo3-gui/neo3-gui/ClientApp/main.js
+++ neo3-gui/neo3-gui/ClientApp/main.js
@@ -15,6 +15,7 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+const gotTheLock = app.requestSingleInstanceLock()
 
 
 // adapted from https://github.com/chentsulin/electron-react-boilerplate
@@ -67,47 +68,63 @@ function createWindow() {
         // when you should delete the corresponding element.
         mainWindow = null
     })
-    
-    mainWindow.webContents.on('new-window',function(event, url){
+
+    mainWindow.webContents.on('new-window', function (event, url) {
         event.preventDefault()
     })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', () => {
-
-    if (process.env.NODE_ENV === 'development') {
-        installExtensions().then(() => createWindow())
-    } else {
-        createWindow()
-    }
-})
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) createWindow()
-})
-
-app.on('web-contents-created', (event, wc) => {
-    wc.on('before-input-event', (event, input) => {
-      // Windows/Linux hotkeys
-      if (process.platform !== 'darwin') {
-        if (input.key === 'F12') {
-          mainWindow.webContents.openDevTools()
-          event.preventDefault()
+if (!gotTheLock) {
+    app.quit()
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        // Someone tried to run a second instance, we should focus our window.
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore()
+            mainWindow.focus()
         }
-      }
     })
-  })
+
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    // Some APIs can only be used after this event occurs.
+    app.on('ready', () => {
+
+        if (process.env.NODE_ENV === 'development') {
+            installExtensions().then(() => createWindow())
+        } else {
+            createWindow()
+        }
+    })
+
+
+
+    // Quit when all windows are closed.
+    app.on('window-all-closed', function () {
+        // On macOS it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') app.quit()
+    })
+
+    app.on('activate', function () {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (mainWindow === null) createWindow()
+    })
+
+    app.on('web-contents-created', (event, wc) => {
+        wc.on('before-input-event', (event, input) => {
+            // Windows/Linux hotkeys
+            if (process.platform !== 'darwin') {
+                if (input.key === 'F12') {
+                    mainWindow.webContents.openDevTools()
+                    event.preventDefault()
+                }
+            }
+        })
+    })
+}
+
+
+
 // In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
