454b7e8edab6c34d09834dea1ca794d6ab5f14f8
Shuai <bfshm@qq.com>
2020-06-04 16:14:22 +0800

    rawtransaction

--- neo3-gui/neo3-gui/Models/Contracts/InstructionInfo.cs
+++ neo3-gui/neo3-gui/Models/Contracts/InstructionInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Neo.VM;
@@ -22,5 +23,28 @@ namespace Neo.Models.Contracts
         public string SystemCallMethod { get; set; }
 
         public string OpDataUtf8String => OpData == null ? null : Encoding.UTF8.GetString(OpData);
+
+
+        public string OpDataPossibleString => ToOpDataString();
+
+
+        private string ToOpDataString()
+        {
+            if (OpData == null)
+            {
+                return null;
+            }
+            if (SystemCallMethod.NotNull())
+            {
+                return SystemCallMethod;
+            }
+            if (OpCode >= OpCode.PUSHINT8 && OpCode <= OpCode.PUSHINT256)
+            {
+                return new BigInteger(OpData).ToString();
+            }
+            var utf8String = OpDataUtf8String;
+            if (utf8String.Any(p => p < '0' || p > 'z')) return OpData.ToHexString();
+            return utf8String;
+        }
     }
 }
