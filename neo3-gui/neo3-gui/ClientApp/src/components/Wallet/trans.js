810df91401b77dd9ec91202510ddf6f11f39f5b3
yanmingxia <18605540702@163.com>
2020-06-15 17:49:45 +0800

    交易列表tab添加

--- neo3-gui/neo3-gui/ClientApp/src/components/Wallet/trans.js
+++ neo3-gui/neo3-gui/ClientApp/src/components/Wallet/trans.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Layout } from 'antd';
-import Sync from '../sync';
+import {  Layout, Row, Col,Tabs } from 'antd';
 import Transaction from '../Transaction/transaction';
 import Untransaction from '../Transaction/untransaction';
-import { withTranslation } from "react-i18next";
+import Sync from '../sync';
+import { withTranslation } from 'react-i18next';
+
+const { Content } = Layout;
+const { TabPane } = Tabs;
 
 @withTranslation()
 class Wallettrans extends React.Component{
@@ -19,8 +22,20 @@ class Wallettrans extends React.Component{
     return (
       <Layout className="gui-container">
         <Sync />
-        <Untransaction content={t("blockchain.transaction.pending")} page="wallet"/>
-        <Transaction content={t("wallet.lastest transactions")} page="wallettrans"/>
+        <Content className="mt3">
+          <Row gutter={[30, 0]} className="mb1">
+            <Col span={24} className="bg-white pv4">
+              <Tabs className="tran-title trans-list-title" defaultActiveKey="1">
+                <TabPane tab={t("blockchain.transactions")} key="1">
+                  <Transaction content={t("blockchain.transactions")} page="all" />
+                </TabPane>
+                <TabPane tab={t("blockchain.transaction.pending")} key="2">
+                  <Untransaction content={t("blockchain.transaction.pending")}/>
+                </TabPane>
+              </Tabs>
+            </Col>
+          </Row>
+        </Content>
       </Layout>
     );
   }
